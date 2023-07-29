addf6154892d2c51e9cfb68b96dbb4f7dcb1e1df
wisteen <wisdomisaac168@gmail.com>
2023-07-28 19:37:34 -0700

    fix

--- control/settings.py
+++ control/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Following settings only make sense on production and may break development environments.
-if DEBUG:
+if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticall')
